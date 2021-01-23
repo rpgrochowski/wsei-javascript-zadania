24e71cbbb98cd179db999e1a4d1787ed1723bb23
rpgrochowski <wiliam1arafal@gmail.com>
2021-01-23 23:11:34 +0100

    Update script.js
    add

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+// zadanie 1
+
+console.log("Rafał");
+
+//zadanie 2
+
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+// zadanie 3
+
+console.log("Hello World"); //pojawia sie string
+console.log(Hello World); // błąd poieważ nie ma zmiennej
+console.log("Hello World); // błąd bo nie ma nawiasu "
