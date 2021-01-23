2c739762cf358d4407ce6b0ee36d4487b7916c25
rpgrochowski <wiliam1arafal@gmail.com>
2021-01-23 23:24:14 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,29 @@
+// zadanie 1
+
+let number1 = 2;
+let text1 = "text";
+let numberAndString = 5 + " trzy";
+let logic = 3 < 2;
+let special = null;
+
+console.log(number1);
+console.log(text1);
+console.log(numberAndString);
+console.log(logic);
+console.log(special);
+
+//zadanie 2
+
+var number1 = 10;
+var number2 = 3;
+var sum = 0;
+
+sum = number1 + number2;
+
+console.log(sum);
+
+//zadanie 3
+
+var number;
+
+console.log(number); //"undefined" nie zadeklarowana zmienna nie posiadada wartości
