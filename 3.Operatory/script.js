c59dd4f77cc0c084b133b1f8e32c292f89b06664
rpgrochowski <wiliam1arafal@gmail.com>
2021-01-23 23:48:11 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,49 @@
+//zadanie 1
+var boolOne = true;
+var boolTwo = false;
+
+console.log(boolOne === boolTwo);
+
+// potrójny sprawdza wartości i typy
+
+//zadanie 2
+var numOne = 10;
+var numTwo = 2;
+var moduloResult = 0;
+
+moduloResult= numOne%numTwo;
+
+console.log(moduloResult);
+
+//zadanie 3
+var stringOne = "Zamek";
+var stringTwo = "Królewski";
+
+var stringsResult = ""
+
+stringsResult = stringOne + " " + stringTwo;
+
+console.log(stringsResult);
+
+//zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString); // porównuje wartosci
+console.log(someNumber === someString); // porównuje wartości i typy
+
+//zadanie 5
+var counter = 30;
+
+console.log(counter);
+console.log(++counter);
+console.log(--counter); 
+
+//zadanie 6
+var numberOne = 10;
+var numberTwo = 4;
+var result = null;
+
+result = numberOne > numberTwo
+
+console.log(result);
